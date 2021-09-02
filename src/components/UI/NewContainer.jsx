f24b642ec64bbbc2404a9b03d3743672685a0c07
bbshark99 <blue.shark99@outlook.com>
2021-09-02 08:48:13 -0500

    Fixed onomy-footer responsive

--- src/components/UI/NewContainer.jsx
+++ src/components/UI/NewContainer.jsx
@@ -13,7 +13,7 @@ export const Container = styled.div`
   }
 
   @media screen and (max-width: ${responsive.laptopSmall}) {
-    padding: 0 10px;
+    padding: 5px 10px;
   }
 
   @media screen and (max-width: ${responsive.smartphoneLarge}) {
